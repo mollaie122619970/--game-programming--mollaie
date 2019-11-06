7814f99be94f0de9f0d7919e323cf063690eb9c4
mollaie122619970 <56757374+mollaie122619970@users.noreply.github.com>
2019-11-06 21:32:59 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                 Console.WriteLine("jomle ra vared konid");
+                 string input2=(Console.ReadLine());
+                 while(input2!=".");
+                 if(input2<=9 && input2>=0)
+                 {
+                 Console.WriteLine(int count1++);
+                 }
+                 else   if(input2<="z" && input2>="a")
+                 {
+                 Console.WriteLine(int count2++);
+                 }
+                 else   if(input2==" ")
+                 {
+                 Console.WriteLine(int count3++);
+                 }
+        }
+
+        }
+    }
+
